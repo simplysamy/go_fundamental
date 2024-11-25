0a0aa089c33c619f9a34f5dcc15635a8ed3ad838
Sameer Hasan <simplysamy@gmail.com>
2024-11-25 10:43:44 +0530

    Creat first go file

---
+++ 1-func_main/main.go
@@ -0,0 +1,9 @@
+// Package must be named main so that Go knows where our application starts
+package main
+
+import "fmt"
+
+//  function must be named main so that Go knows which code to execute when the application starts
+func main() {
+	fmt.Print("Hello World")
+}
