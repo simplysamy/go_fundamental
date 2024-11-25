8a71dd90f1fa20ef9ea99f1a05e10617a409363e
Sameer Hasan <simplysamy@gmail.com>
2024-11-25 10:54:22 +0530

    unsigned integer

--- 1-func_main/main.go
+++ 1-func_main/main.go
@@ -3,13 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	// Integer Types
-	var integerNum int = 42 // Platform dependent (32 or 64 bit)
-	// var int8Num int8 = 127          // -128 to 127
-	// var int16Num int16 = 32767      // -32768 to 32767
-	// var int32Num int32 = 2147483647 // -2147483648 to 2147483647
-	// var int64Num int64 = 9223372036854775807
 
-	fmt.Printf("Integer: %v\n", integerNum) // %v is default format
-	fmt.Printf("Integer: %d\n", integerNum) // %d is integer format
+	// Unsigned Integer Types
+	var uintNum uint = 42 // Platform dependent
+	//   var uint8Num uint8 = 255       // 0 to 255
+	//   var uint16Num uint16 = 65535   // 0 to 65535
+	//   var uint32Num uint32 = 4294967295
+
+	fmt.Printf("Unsigned Integer: %v\n", uintNum)
 }
