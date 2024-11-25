da6a8c63f3c06663a4aa1d0cddd85822173fb6ae
Sameer Hasan <simplysamy@gmail.com>
2024-11-25 11:04:35 +0530

    Bollean Value

--- 1-func_main/main.go
+++ 1-func_main/main.go
@@ -4,10 +4,7 @@ import "fmt"
 
 func main() {
 
-	// Float Types
-	var float32Num float32 = 3.14
-	var float64Num float64 = 3.141592653589793
-
-	fmt.Printf("Float: %f\n", float32Num)
-	fmt.Printf("Float: %v\n", float64Num)
+	var isTrue bool = true
+	var isFalse bool = false
+	fmt.Println(isTrue, isFalse)
 }
