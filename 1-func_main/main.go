c4bae4722ac84e359dc22f2ff2a263e850439d3e
Sameer Hasan <simplysamy@gmail.com>
2024-11-25 10:50:57 +0530

    signed integer

--- 1-func_main/main.go
+++ 1-func_main/main.go
@@ -1,9 +1,15 @@
-// Package must be named main so that Go knows where our application starts
 package main
 
 import "fmt"
 
-//  function must be named main so that Go knows which code to execute when the application starts
 func main() {
-	fmt.Print("Hello World")
+	// Integer Types
+	var integerNum int = 42 // Platform dependent (32 or 64 bit)
+	// var int8Num int8 = 127          // -128 to 127
+	// var int16Num int16 = 32767      // -32768 to 32767
+	// var int32Num int32 = 2147483647 // -2147483648 to 2147483647
+	// var int64Num int64 = 9223372036854775807
+
+	fmt.Printf("Integer: %v\n", integerNum) // %v is default format
+	fmt.Printf("Integer: %d\n", integerNum) // %d is integer format
 }
