b725f4a88e9e72f4208531615198c9b7794767d2
Sameer Hasan <simplysamy@gmail.com>
2024-11-25 11:09:50 +0530

    fix size array

--- 1-func_main/main.go
+++ 1-func_main/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	// Fixed-size array
+	var numbers [5]int = [5]int{1, 2, 3, 4, 5}
 
-	var isTrue bool = true
-	var isFalse bool = false
-	fmt.Println(isTrue, isFalse)
+	fmt.Printf("Numbers: %v\n", numbers)
 }
