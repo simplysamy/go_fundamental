443ceba53e29a85332aa35fa6201be236cfcfac9
Sameer Hasan <simplysamy@gmail.com>
2024-11-26 14:28:46 +0530

    Putting variables of same types in one line

--- 2-investment_cal/main.go
+++ 2-investment_cal/main.go
@@ -6,8 +6,7 @@ import (
 )
 
 func main() {
-	investmentAmount, years := 1000.0, 10.0
-	expectedReturnRate := 5.5
+	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
 
 	futureValue := (investmentAmount) * math.Pow(1+expectedReturnRate/100, (years))
 	fmt.Println(futureValue)
