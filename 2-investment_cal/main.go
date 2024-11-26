227b71360d4ac56c5a561791ed97f61e19b90366
Sameer Hasan <simplysamy@gmail.com>
2024-11-26 14:09:08 +0530

    intial code

---
+++ 2-investment_cal/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+
+	var futureValue = investmentAmount * (1 + expectedReturnRate/100) * *years
+}
