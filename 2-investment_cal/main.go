903d1d5b70f459d6f7ce9564fb764b399b22e31b
Sameer Hasan <simplysamy@gmail.com>
2024-11-26 14:32:20 +0530

    Add constant inflation rate

--- 2-investment_cal/main.go
+++ 2-investment_cal/main.go
@@ -6,10 +6,12 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
+
 	var investmentAmount float64 = 1000
 	expectedReturnRate := 5.5
 	years := 10.0
 
-	var futureValue = (investmentAmount) * math.Pow(1+expectedReturnRate/100, (years))
+	futureValue := (investmentAmount) * math.Pow(1+expectedReturnRate/100, (years))
 	fmt.Println(futureValue)
 }
