a78ccd0c1c1151e03eb33d1874167048736a0b57
Sameer Hasan <simplysamy@gmail.com>
2024-11-26 14:30:31 +0530

    Return to the previous method

--- 2-investment_cal/main.go
+++ 2-investment_cal/main.go
@@ -6,8 +6,10 @@ import (
 )
 
 func main() {
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	var investmentAmount float64 = 1000
+	expectedReturnRate := 5.5
+	years := 10.0
 
-	futureValue := (investmentAmount) * math.Pow(1+expectedReturnRate/100, (years))
+	var futureValue = (investmentAmount) * math.Pow(1+expectedReturnRate/100, (years))
 	fmt.Println(futureValue)
 }
