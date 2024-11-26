2dc49c3c57f6d2d7dd96d9fe0ba31d2cea094fbd
Sameer Hasan <simplysamy@gmail.com>
2024-11-26 14:20:43 +0530

    math package pow

--- 2-investment_cal/main.go
+++ 2-investment_cal/main.go
@@ -1,12 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
 	var investmentAmount = 1000
 	var expectedReturnRate = 5.5
 	var years = 10
 
-	//change investmentAmount to float64
-	//expectedReturnRate is in float64
-
-	var futureValue = float64(investmentAmount) * (1 + expectedReturnRate/100) * *float64(years)
+	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	fmt.Println(futureValue)
 }
